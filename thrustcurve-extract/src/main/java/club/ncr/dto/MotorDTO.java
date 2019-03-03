6b667a7e2664ad2dc2a92699957ca76651b4d638
Brad <brad@morsecode-inc.com>
2019-03-03 14:07:27 -0700

    fixing

--- thrustcurve-extract/src/main/java/club/ncr/dto/MotorDTO.java
+++ thrustcurve-extract/src/main/java/club/ncr/dto/MotorDTO.java
@@ -8,37 +8,49 @@ import java.util.stream.Collectors;
 
 public class MotorDTO {
 
-    public final MotorManufacturerDTO manufacturer;
+    public final String name;
     public final String designation;
+    public final String identifier;
     public final String motorCase;
     public final String impulse;
     public final Double weight;
     public final Double burnTime;
     public final Double diameter;
+    public final Double length;
     public final String externalId;
+    public final String propellant;
+    public final MotorManufacturerDTO manufacturer;
     public final List<MotorDataDTO> data;
 
     public MotorDTO(Motor motor) {
+        this.name= motor.getBrandName();
+        this.identifier= motor.getDesignation() +" ["+ motor.getManufacturer().getAbbreviation() +"]";
+        this.length= motor.getLength();
         this.externalId = motor.getExternalID();
         this.manufacturer= new MotorManufacturerDTO(motor.getManufacturer());
         this.designation= motor.getDesignation();
         this.motorCase= motor.getCase().getName();
         this.impulse= motor.getImpulse().getImpulse();
-        this.weight = motor.getWeight();
-        this.burnTime = motor.getBurnTime();
+        this.weight= motor.getWeight();
+        this.burnTime= motor.getBurnTime();
         this.diameter= (double)motor.getDiameter().getDiameter();
-        this.data= motor.getData().stream().map(d -> new MotorDataDTO(motor, d)).collect(Collectors.toList());
+        this.propellant= motor.getPropellant().getName();
+        this.data= motor.getData().stream().map(d -> new MotorDataDTO(this, d)).collect(Collectors.toList());
     }
 
     public MotorDTO(TCMotorRecord motor) {
+        this.name= motor.getBrandName();
+        this.identifier= motor.getDesignation() +" ["+ motor.getManufacturerAbbv() +"]";
         this.externalId = motor.getMotorId();
         this.manufacturer= new MotorManufacturerDTO(motor);
         this.designation= motor.getDesignation();
+        this.length= motor.getLength();
         this.motorCase= motor.getMotorCase();
         this.impulse= motor.getImpulseClass();
         this.weight = motor.getWeight();
         this.burnTime = motor.getBurnTime();
         this.diameter= Double.parseDouble(motor.getDiameter());
-        this.data= motor.getData().stream().map(d -> new MotorDataDTO(motor, d)).collect(Collectors.toList());
+        this.propellant= motor.getPropellant();
+        this.data= motor.getData().stream().map(d -> new MotorDataDTO(this, d)).collect(Collectors.toList());
     }
 }
--- thrustcurve-extract/src/main/java/club/ncr/dto/MotorDataDTO.java
+++ thrustcurve-extract/src/main/java/club/ncr/dto/MotorDataDTO.java
@@ -1,47 +1,32 @@
 package club.ncr.dto;
 
-import club.ncr.cayenne.Motor;
 import club.ncr.cayenne.MotorData;
 import org.thrustcurve.api.data.TCMotorData;
-import org.thrustcurve.api.data.TCMotorRecord;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class MotorDataDTO {
 
-    public final MotorDTO motor;
     public final String dataUrl;
     public final String infoUrl;
     public final String source;
     public final String license;
     public final String fileExtension;
-    public final String impulse;
-    public final Double burnTime;
-    public final Float diameter;
 
 
-    public MotorDataDTO(Motor motor, MotorData data) {
-        this.motor= new MotorDTO(motor);
+    // these constructors are only visible to the package on purpose, this is to force the instantiation of a MotorDTO first.
+    MotorDataDTO(MotorDTO motor, MotorData data) {
         this.dataUrl = data.getDataUrl();
         this.infoUrl = data.getInfoUrl();
         this.source= data.getSource();
         this.license= data.getLicense();
         this.fileExtension = data.getFormat().getFileExtension();
-        this.impulse= motor.getImpulse().getImpulse();
-        this.burnTime = motor.getBurnTime();
-        this.diameter= motor.getDiameter().getDiameter();
     }
 
-    public MotorDataDTO(TCMotorRecord motor, TCMotorData data) {
-        this.motor= new MotorDTO(motor);
+    MotorDataDTO(MotorDTO motor, TCMotorData data) {
         this.dataUrl = data.getDataUrl();
         this.infoUrl = data.getInfoUrl();
         this.source= data.getSource();
         this.license= data.getLicense();
         this.fileExtension = data.getFormat();
-        this.impulse= motor.getImpulseClass();
-        this.burnTime = motor.getBurnTime();
-        this.diameter= BigDecimal.valueOf(Float.parseFloat(motor.getDiameter())).setScale(1, RoundingMode.HALF_EVEN).floatValue();
     }
 }
