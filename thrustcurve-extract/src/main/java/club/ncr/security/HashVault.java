acd98c4f20b97d5739cb7bd4394fd22273e1b8e1
Brad <brad@morsecode-inc.com>
2019-04-19 00:40:45 -0600

    using java.util.Base64

--- thrustcurve-extract/src/main/java/club/ncr/security/HashVault.java
+++ thrustcurve-extract/src/main/java/club/ncr/security/HashVault.java
@@ -1,9 +1,6 @@
 package club.ncr.security;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
-import java.io.IOException;
+import java.util.Base64;
 
 public class HashVault {
 
@@ -21,42 +18,31 @@ public class HashVault {
         String encoded = Crypto.encode(salted);
         String key = cypher(encoded);
         return key;
-        //return cypher(Crypto.encode(salted(escape(value))));
     }
 
     private static String decypher(String key) {
-        try {
-            return new String(new BASE64Decoder().decodeBuffer(key));
-        } catch (IOException iox) {
-            //throw new Exception("Failed "+ key +" ["+ iox.getMessage() +"]", iox);
-            return null;
-        }
+        return new String(Base64.getDecoder().decode(key));
     }
 
     private static String salted(String key) {
         String salted= System.currentTimeMillis() +"|" + key;
-        //System.out.println(" + salt("+ key +"): "+ salted);
         return salted;
     }
 
     private static String unsalted(String key) {
-        //System.out.println(" - unsalt("+ key +")");
         return (key.substring(key.indexOf("|") + 1));
     }
 
     private static String cypher(String key) {
-        String cypher = new String(new BASE64Encoder().encode(key.getBytes()));
-        //System.out.println(" + cypher("+ key +"): "+ cypher);
+        String cypher = new String(Base64.getEncoder().encode(key.getBytes()));
         return cypher;
     }
 
     private static String escape(String key) {
-        //System.out.println(" + escape("+ key +")");
         return key;
     }
 
     private static String unescape(String key) {
-        //System.out.println(" - unescape("+ key +")");
         return key;
     }
 
