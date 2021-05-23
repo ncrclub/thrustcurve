2bb73c2f2060c2b1cd1bd56b24bbfb95cd15aaaa
Brad M <brad@morsecode-inc.com>
2021-05-22 18:24:11 -0600

    max impulse = O

--- thrustcurve-extract/src/main/java/club/ncr/dto/motor/ImpulseDTO.java
+++ thrustcurve-extract/src/main/java/club/ncr/dto/motor/ImpulseDTO.java
@@ -22,18 +22,18 @@ public enum ImpulseDTO {
     L("L", (int)(2.0 * K.maxNewtonSeconds)),
     M("M", (int)(2.0 * L.maxNewtonSeconds)),
     N("N", (int)(2.0 * M.maxNewtonSeconds)),
-    O("O", (int)(2.0 * N.maxNewtonSeconds)),
-    P("P", (int)(2.0 * O.maxNewtonSeconds)),
-    Q("Q", (int)(2.0 * P.maxNewtonSeconds)),
-    R("R", (int)(2.0 * Q.maxNewtonSeconds)),
-    S("S", (int)(2.0 * R.maxNewtonSeconds)),
-    T("T", (int)(2.0 * S.maxNewtonSeconds)),
-    U("U", (int)(2.0 * T.maxNewtonSeconds)),
-    V("V", (int)(2.0 * U.maxNewtonSeconds)),
-    W("W", (int)(2.0 * V.maxNewtonSeconds)),
-    X("X", (int)(2.0 * W.maxNewtonSeconds)),
-    Y("Y", (int)(2.0 * X.maxNewtonSeconds)),
-    Z("Z", (int)(2.0 * Y.maxNewtonSeconds))
+    O("O", (int)(2.0 * N.maxNewtonSeconds))
+    // P("P", (int)(2.0 * O.maxNewtonSeconds)),
+    // Q("Q", (int)(2.0 * P.maxNewtonSeconds)),
+    // R("R", (int)(2.0 * Q.maxNewtonSeconds)),
+    // S("S", (int)(2.0 * R.maxNewtonSeconds)),
+    // T("T", (int)(2.0 * S.maxNewtonSeconds)),
+    // U("U", (int)(2.0 * T.maxNewtonSeconds)),
+    // V("V", (int)(2.0 * U.maxNewtonSeconds)),
+    // W("W", (int)(2.0 * V.maxNewtonSeconds)),
+    // X("X", (int)(2.0 * W.maxNewtonSeconds)),
+    // Y("Y", (int)(2.0 * X.maxNewtonSeconds)),
+    // Z("Z", (int)(2.0 * Y.maxNewtonSeconds))
     ;
 
     public final double minNewtonSeconds;
@@ -54,7 +54,7 @@ public enum ImpulseDTO {
                 return impulse;
             }
         }
-        return Z;
+        return O;
     }
 
     public static ImpulseDTO valueOf(MotorImpulse impulse) {
